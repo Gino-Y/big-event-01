51b072f0092b0f356a66c7c80968496035f0e26a
Gino-Y <ginoyan250@gmail.com>
2023-12-10 00:19:53 +0800

    文章分类模块开发

--- src/main/java/com/itheima/pojo/Category.java
+++ src/main/java/com/itheima/pojo/Category.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 public class Category {
     private Integer id;
     private String categoryName;
+    private String categoryAlias;
     private Integer createUser;
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
