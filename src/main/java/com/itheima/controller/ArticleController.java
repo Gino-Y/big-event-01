56a2a3e73b4fd885e22695f7e9709af9239199c4
Gino-Y <ginoyan250@gmail.com>
2023-12-10 13:05:53 +0800

    文章管理模块

--- src/main/java/com/itheima/controller/ArticleController.java
+++ src/main/java/com/itheima/controller/ArticleController.java
@@ -1,29 +1,38 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Article;
 import com.itheima.pojo.Result;
+import com.itheima.service.ArticleService;
 import com.itheima.utils.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
-    @GetMapping("/list")
-    public Result<String> list(/*@RequestHeader(name="Authorization") String token, HttpServletResponse response*/) {
-//        // 验证token
-//        try {
-//            Map<String, Object> claims = JwtUtil.parseToken(token);
-//            return Result.success("文章列表");
-//        } catch (Exception e) {
-//            // http状态码：401
-//            response.setStatus(401);
-//            return Result.error("未登录");
-//        }
-        return Result.success("所有文章数据...");
+//    @GetMapping("/list")
+//    public Result<String> list(/*@RequestHeader(name="Authorization") String token, HttpServletResponse response*/) {
+////        // 验证token
+////        try {
+////            Map<String, Object> claims = JwtUtil.parseToken(token);
+////            return Result.success("文章列表");
+////        } catch (Exception e) {
+////            // http状态码：401
+////            response.setStatus(401);
+////            return Result.error("未登录");
+////        }
+//        return Result.success("所有文章数据...");
+//    }
+
+    @Autowired
+    private ArticleService articleService;
+
+    @PostMapping
+    public Result add(@RequestBody Article article){
+        articleService.add(article);
+        return Result.success();
     }
 }
--- src/main/java/com/itheima/service/ArticleService.java
+++ src/main/java/com/itheima/service/ArticleService.java
@@ -1,4 +1,8 @@
 package com.itheima.service;
 
+import com.itheima.pojo.Article;
+
 public interface ArticleService {
+    // 添加文章
+    void add(Article article);
 }
