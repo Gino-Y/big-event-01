2be9033066fa269ff4ec0e64c69320b99b72e9d4
Gino-Y <ginoyan250@gmail.com>
2023-12-10 12:48:38 +0800

    文章管理模块

---
+++ src/main/java/com/itheima/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.ArticleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+}
